5cf41f3cab046963ef78f5fad36be688248cdf64
Abdelrahman mustafa <92968146+bodaHadsvds@users.noreply.github.com>
2023-06-26 15:54:35 +0200

    search by emails

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css'
 // import { robots } from "./robots";
 import Scroll from "./scroll";
 import Errorboundry from "./Errorboundry";
+import { robots } from "./robots";
 
 
 class App extends Component {
@@ -23,10 +24,12 @@ class App extends Component {
   componentDidMount(){
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(Response=>{
+      
     return  Response.json()
 
     })
     .then(users=>{
+      console.log("response",users )
         this.setState({robots:users})
     })
 
@@ -42,12 +45,18 @@ class App extends Component {
     const filterdrobots = this.state.robots.filter(robots=> {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
   })
+  const filteredemails = this.state.robots.filter(robots=> {
+    return robots.email.toLowerCase().includes(this.state.searchfield.toLowerCase())
+})
+console.log(robots.email)
+ const filterdrobos = filterdrobots.concat(filteredemails)
+
   if(this.state.robots.length === 0)  { return  <h1>loading</h1> 
   }  else {return (
     <div className="tc" > 
     <h1 className="f2"> Robofriends </h1>
     <SearchBox  searchchange={this.onsearchchange}/>
-    <Scroll>    <Errorboundry> <Cardlist robots={filterdrobots} /></Errorboundry>       </Scroll>
+    <Scroll>    <Errorboundry> <Cardlist robots={filterdrobos} /></Errorboundry>       </Scroll>
    </div>
 
    )
